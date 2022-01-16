243d9baac3d222d8ef8aebccc9d7b94cbc781bbd
Alex Puz <alex.puz@yahoo.com>
2022-01-16 09:14:56 -0500

    Nesting HTTPClientResult in to `HTTPClient`, using standard `Result` type

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -88,8 +88,8 @@ class URLSessionHTTPClientTests: XCTestCase {
 		let result = resultFor(data: data, response: response, error: error, file: file, line: line)
 
 		switch result {
-		case let .success(data, response):
-			return (data, response)
+		case let .success(result):
+			return result
 		default:
 			XCTFail("Expected success, got \(result) instead", file: file, line: line)
 			return nil
@@ -109,12 +109,12 @@ class URLSessionHTTPClientTests: XCTestCase {
 		}
 	}
 	
-	private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> HTTPClientResult {
+	private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) -> HTTPClient.Result {
 		URLProtocolStub.stub(data: data, response: response, error: error)
 		let sut = makeSUT(file: file, line: line)
 		let exp = expectation(description: "Wait for completion")
 		
-		var receivedResult: HTTPClientResult!
+		var receivedResult: HTTPClient.Result!
 		sut.get(from: anyURL()) { result in
 			receivedResult = result
 			exp.fulfill()
