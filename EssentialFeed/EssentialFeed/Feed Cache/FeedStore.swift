82414e552f73c6a64404bb28c61ffb301d720012
Alex Puz <alex.puz@yahoo.com>
2022-01-16 09:55:39 -0500

    Move `RetrieveCachedFeedResult` to `FeedStore`, use the standard `Result` type

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -4,16 +4,18 @@
 
 import Foundation
 
-public enum RetrieveCachedFeedResult {
-	case empty
-	case found(feed: [LocalFeedImage], timestamp: Date)
-	case failure(Error)
+public enum CacheFeed {
+    case empty
+    case found(feed: [LocalFeedImage], timestamp: Date)
 }
 
 public protocol FeedStore {
+    
+    typealias RetrievalResult = Result<CacheFeed, Error>
+    
 	typealias DeletionCompletion = (Error?) -> Void
 	typealias InsertionCompletion = (Error?) -> Void
-	typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
+	typealias RetrievalCompletion = (RetrievalResult) -> Void
 
 	/// The completion handler can be invoked in any thread.
 	/// Clients are responsible to dispatch to appropriate threads, if needed.
